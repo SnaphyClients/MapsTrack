249a0c7fc988614083afccc82afd417b530b6f5f
ravi gupta <ravigupta9363@gmail.com>
2016-08-18 10:41:37 +0530

    Internal color theme has been changed

--- app/src/main/java/com/snaphy/mapstrack/MainActivity.java
+++ app/src/main/java/com/snaphy/mapstrack/MainActivity.java
@@ -1903,7 +1903,7 @@ public class MainActivity extends AppCompatActivity implements OnFragmentChange,
     public CharSequence drawTextViewDesign(String constant, String data) {
         SpannableString spannableString =  new SpannableString(constant);
         SpannableString spannableString2 =  new SpannableString(data);
-        spannableString.setSpan(new ForegroundColorSpan(Color.rgb(63, 81, 181)),0,constant.length(),0);
+        spannableString.setSpan(new ForegroundColorSpan(Color.rgb(237, 97, 116)),0,constant.length(),0);
         spannableString.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD), 0, constant.length(), 0);
         spannableString.setSpan(new RelativeSizeSpan(1.1f), 0, constant.length(), 0);
         CharSequence result = (TextUtils.concat(spannableString, " ", spannableString2));
